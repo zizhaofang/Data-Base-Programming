6aed790d2eb94db38df7cb91e90b322735c3a08c
Zizhao Fang <zizhao.fang@duke.edu>
2018-03-01 21:17:42 -0500

    Connect_test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,53 @@
 using namespace std;
 using namespace pqxx;
 
+
+void createTable(connection* C) {
+  string sql;
+  string dropT[4] = {
+    "DROP TABLE IF EXISTS PLAYER;",
+    "DROP TABLE IF EXISTS TEAM;",
+    "DROP TABLE IF EXISTS STATE;",
+    "DROP TABLE IF EXISTS COLOR;" };
+  string createT[4] = {
+    "CREATE TABLE PLAYER (
+      PLAYER_ID INT PRIMARY KEY NOT NULL, 
+      TEAM_ID INT NOT NULL REFERENCES TEAM(TEAM_ID), 
+      UNIFORM_NUM INT NOT NULL, 
+      FIRST_NAME TEXT NOT NULL, 
+      LAST_NAME TEXT NOT NULL, 
+      MPG INT NOT NULL, 
+      PPG INT NOT NULL, 
+      RPG INT NOT NULL, 
+      APG INT NOT NULL, 
+      SPG NUMERIC(2,1) NOT NULL, 
+      BPG NUMERIC(2,1) NOT NULL);",
+    "CREATE TABLE TEAM (
+      TEAM_ID INT PRIMARY KEY NOT NULL,
+      NAME TEXT NOT NULL,
+      STATE_ID INT NOT NULL REFERENCES STATE(STATE_ID),
+      COLOR_ID INT NOT NULL REFERENCES COLOR(COLOR_ID),
+      WINS INT NOT NULL,
+      LOSSES INT NOT NULL
+    );",
+    "CREATE TABLE STATE(
+      STATE_ID INT PRIMARY KEY NOT NULL,
+      NAME TEXT NOT NULL
+    );",
+    "CREATE TABLE COLOR(
+      COLOR_ID INT PRIMARY KEY NOT NULL,
+      NAME TEXT NOT NULL
+    );"
+  };
+  for(int i = 0; i < 4 ; i++) {
+    sql += dropT[i] + createT[i];
+  }
+  work W(*C);
+  W.exec( sql );
+  W.commit();
+  cout << "Table created successfully" << endl;
+}
+
 int main (int argc, char *argv[]) 
 {
 
@@ -27,10 +74,15 @@ int main (int argc, char *argv[])
     return 1;
   }
 
-
   //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
   //      load each table with rows from the provided source txt files
-
+  /////////////////// Create table ///////////////////////////
+  try{
+    createTable(C);
+  } catch (const std::exception &e){
+    cerr << e.what() << std::endl;
+    return 1;
+  }
 
   exercise(C);
 
